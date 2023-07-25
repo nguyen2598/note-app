9114586f9ceabb1876059f2e0f73f56be909e46f
nguyen2598 <dinhnguyen250802@gmail.com>
2023-07-25 17:23:07 +0700

    success

--- server/src/app/config/db.js
+++ server/src/app/config/db.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect(
-            'mongodb+srv://devblog:devblog@cluster0.qriwtnt.mongodb.net/Devblog?retryWrites=true&w=majority' /*process.env.URL_DATABASE */,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                // useCreateIndex: true,
-            },
-        );
+        await mongoose.connect(process.env.URL_DATABASE, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useCreateIndex: true,
+        });
         console.log('database succcessfully');
     } catch (error) {}
 }
