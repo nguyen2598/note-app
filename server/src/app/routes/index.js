7ea76d7f6762e6ad2d8a8f5a7d7eb855a2305e0e
nguyen2598 <dinhnguyen250802@gmail.com>
2023-07-25 10:59:35 +0700

    update db

--- server/src/app/routes/index.js
+++ server/src/app/routes/index.js
@@ -4,6 +4,9 @@ const postRouter = require('./post');
 function route(app) {
     app.use('/auth', authRouter);
     app.use('/posts', postRouter);
+    app.use('/', (req, res, next) => {
+        res.send('server này có chạy nhá');
+    });
 }
 
 module.exports = route;
