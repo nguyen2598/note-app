8fa3e2b68a9538018b293dd7838f070cad9c053a
nguyen2598 <dinhnguyen250802@gmail.com>
2023-07-25 12:31:05 +0700

    connect db

--- server/src/app/config/db.js
+++ server/src/app/config/db.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect(
-            'mongodb+srv://noteapp:noteapp@cluster0.2hmljmw.mongodb.net/note?retryWrites=true&w=majority',
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                // useCreateIndex: true,
-            },
-        );
+        await mongoose.connect(process.env.URL_DATABASE, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useCreateIndex: true,
+        });
         console.log('database succcessfully');
     } catch (error) {}
 }
